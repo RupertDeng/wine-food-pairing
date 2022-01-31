66485f4668eb4b09a3c356fecbb00f84d7eb9019
Rupert Deng <rupert.deng@gmail.com>
2022-01-31 13:02:52 -0500

    add wine data set processing

--- data_importer.py
+++ data_importer.py
@@ -15,6 +15,8 @@ def import_wine_data():
   wine_raw_data.drop_duplicates(subset=['Name'], inplace=True)
   for geo in ['Subregion', 'Region', 'Province', 'Country']:
     wine_raw_data[geo] = wine_raw_data[geo].apply(lambda g: str(g).strip())
+
+  wine_raw_data.drop(wine_raw_data.columns[-1], axis=1, inplace=True)
   
   return wine_raw_data
 
@@ -28,3 +30,26 @@ def import_food_data():
 
 def import_descriptor_mapping():
   return pd.read_csv('references/descriptor_mapping.csv', encoding='latin1').set_index('raw descriptor')
+
+
+def import_variety_mapping():
+  variety_mapping = {'Shiraz': 'Syrah', 'Pinot Gris': 'Pinot Grigio', 'Pinot Grigio/Gris': 'Pinot Grigio',
+  'Garnacha, Grenache': 'Grenache', 'Garnacha': 'Grenache', 'CarmenÃ¨re': 'Carmenere',
+  'GrÃ¼ner Veltliner': 'Gruner Veltliner', 'TorrontÃ©s': 'Torrontes', 
+  'RhÃ´ne-style Red Blend': 'Rhone-style Red Blend', 'AlbariÃ±o': 'Albarino',
+  'GewÃ¼rztraminer': 'Gewurztraminer', 'RhÃ´ne-style White Blend': 'Rhone-style White Blend',
+  'SpÃƒÂ¤tburgunder, Pinot Noir': 'Pinot Noir', 'Sauvignon, Sauvignon Blanc': 'Sauvignon Blanc',
+  'Pinot Nero, Pinot Noir': 'Pinot Noir', 'Malbec-Merlot, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend',
+  'Meritage, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend', 'Garnacha, Grenache': 'Grenache',
+  'FumÃ© Blanc': 'Sauvignon Blanc', 'Cabernet Sauvignon-Cabernet Franc, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend',
+  'Cabernet Merlot, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend', 'Cabernet Sauvignon-Merlot, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend',
+  'Cabernet Blend, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend', 'Malbec-Cabernet Sauvignon, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend',
+  'Merlot-Cabernet Franc, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend', 'Merlot-Cabernet Sauvignon, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend',
+  'Cabernet Franc-Merlot, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend', 'Merlot-Malbec, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend',
+  'Cabernet, Bordeaux-style Red Blend': 'Bordeaux-style Red Blend', 'Primitivo, Zinfandel': 'Zinfandel',
+  'AragonÃªs, Tempranillo': 'Aragonez, Tempranillo'}
+  return variety_mapping
+
+
+def import_normalized_geo_data():
+  return pd.read_csv('references/varieties_all_geos_normalized.csv', index_col=0)
---
+++ step2_prepare_wine_data_set.py
@@ -0,0 +1,77 @@
+from data_importer import import_wine_data, import_variety_mapping, import_normalized_geo_data
+import pandas as pd
+
+
+def cleanup_variety(df, mapping):
+  df['Variety'] = df['Variety'].map(lambda v: mapping.get(v, v))
+  return df
+
+
+def replace_nan_with_none(value):
+  if str(value) in ['0', 'nan']:
+    return 'none'
+  else:
+    return value
+
+
+def cleanup_geo(df):
+  order_of_geographies = ['Subregion', 'Region', 'Province', 'Country']
+  for geo in order_of_geographies:
+    df[geo] = df[geo].map(replace_nan_with_none)
+  df.loc[:, order_of_geographies].fillna('none', inplace=True)
+  return df
+
+
+def consolidate_variety_geo(df, mapping):
+  merge_columns = ['Variety', 'Country', 'Province', 'Region', 'Subregion']
+  df = pd.merge(left=df, right=mapping, left_on=merge_columns, right_on=merge_columns)
+  df = df[['Name', 'Variety', 'geo_normalized', 'Description']]
+  return df
+
+
+def trim_by_variety_geo_frequency(df, freq):
+  trim_columns = ['Variety', 'geo_normalized']
+  variety_geos = df.groupby(trim_columns).size().reset_index().rename(columns={0: 'count'})
+  variety_geos = variety_geos.loc[variety_geos['count'] > freq]
+  trimmed_df = pd.merge(left=df, right=variety_geos, left_on=trim_columns, right_on=trim_columns).drop(['count'], axis=1)
+  return trimmed_df
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+  # import raw wine data as dataframe
+  wine_df = import_wine_data()
+  
+  # map the Variety values to normalized terms
+  variety_mapping = import_variety_mapping()
+  wine_df = cleanup_variety(wine_df, variety_mapping)
+
+  # set empty geo cell to 'none'
+  wine_df = cleanup_geo(wine_df)
+
+  # map the geography columns to a single column of normalized geo and remove other columns not needed
+  normalized_geo_df = import_normalized_geo_data()
+  wine_df = consolidate_variety_geo(wine_df, normalized_geo_df)
+
+  # only keep (variety + geo) which appears more frequently 
+  wine_df = trim_by_variety_geo_frequency(wine_df, 30)
+
+  print(wine_df)
+  
+  
+
+
+  
+
+
+  
+
+
+
+
+
