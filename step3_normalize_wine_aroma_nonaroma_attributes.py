03df2112eb47b950b1b0c9e736574da160da9eb8
Rupert Deng <rupert.deng@gmail.com>
2022-02-06 19:50:47 -0500

    complete wine data processing

--- step3_normalize_wine_aroma_nonaroma_attributes.py
+++ step3_normalize_wine_aroma_nonaroma_attributes.py
@@ -4,6 +4,7 @@ import numpy as np
 from dask_multiprocessing import dask_compute
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
+from collections import Counter
 
 
 def convert_descriptor_to_vector(descriptors, tfidf_weighting, word2vec_model):
@@ -35,30 +36,45 @@ def vectorize_taste_dataframe(df, col, tfidf_weighting, word2vec_model):
 
 
 def get_limited_taste_scalar(variety_df, taste):
+  """
+  for limited taste, simply get the percentage of reviews mentioning this taste for each variety
+  """
   total_review = variety_df[taste].size
   mentioned_review = variety_df[taste].loc[variety_df[taste] != ''].size
   return mentioned_review / total_review
 
-def get_most_freq_descriptor(variety_df, taste):
-  pass
+
+def get_most_freq_aroma_descriptor(variety_df):
+  """
+  for aroma descriptors, get the 50 most common words and their percentage in all descriptors
+  """
+  all_descriptors = []
+  for desc in variety_df['aroma_descriptor']:
+    if desc == '': continue
+    all_descriptors.extend(desc.split(' '))
+  desc_freqs = Counter(all_descriptors)
+  most_common_desc = desc_freqs.most_common(50)
+  return [(desc[0], '{:.2f}'.format(desc[1] / len(all_descriptors))) for desc in most_common_desc]
 
 
 def get_variety_vectors_descriptors(variety_geo, wine_df, core_tastes, limited_taste):
-  variety_df = wine_df.loc[(wine_df['Variey'] == variety_geo[0]) & (wine_df['geo_normalized'] == variety_geo[1])]
+  """
+  for each variety-geo combination, return a row of data including the most_common aroma descriptors, and average vector for each taste.
+  limited-taste will be treated differently, see above.
+  """
+  variety_df = wine_df.loc[(wine_df['Variety'] == variety_geo[0]) & (wine_df['geo_normalized'] == variety_geo[1])]
   output_df = pd.DataFrame({'Variety': [variety_geo[0]], 'Geo': [variety_geo[1]]})
   for taste in core_tastes:
     if taste in limited_taste:
       col_name = taste + ' scalar'
       output_df[col_name] = [get_limited_taste_scalar(variety_df, taste)]
     else:
-      vec_col = taste + ' vector'
-      output_df[vec_col] = [np.average(list(variety_df[taste]), axis=0)]
       if taste == 'aroma':
         desc_col = taste + ' descriptors'
-        output_df[desc_col] = [get_most_freq_descriptor(variety_df, taste)]
+        output_df[desc_col] = [get_most_freq_aroma_descriptor(variety_df)]
+      vec_col = taste + ' vector'
+      output_df[vec_col] = [np.average(list(variety_df[taste]), axis=0)]
   return output_df
-
-
 
 
 if __name__ == '__main__':
@@ -73,6 +89,7 @@ if __name__ == '__main__':
   core_tastes = ['aroma', 'weight', 'sweet', 'acid', 'salt', 'piquant', 'fat', 'bitter']
   limited_tastes = ['salt', 'piquant', 'fat']
 
+  # for each taste, convert the descriptors to a weighted word-embedding vector
   for taste in core_tastes:
     if taste in limited_tastes: continue    # only process non-limited tastes in this block
     taste_words = wine_df[taste]
@@ -82,9 +99,33 @@ if __name__ == '__main__':
     tfidf_weighting_dict = dict(zip(V.get_feature_names_out(), V.idf_))
     wine_df = dask_compute(wine_df, 256, 16, vectorize_taste_dataframe, taste, tfidf_weighting_dict, word2vec_model)
     
+    # for empty cell of non-limited tastes, simply fill in with the average vector from all views for that taste
     non_zero_vec = list(wine_df[taste].dropna())
     avg_vec = np.average(non_zero_vec, axis=0)
     wine_df[taste] = [avg_vec if 'numpy' not in str(type(r)) else r for r in wine_df[taste]]
+
+  # now, we need to converge all data for each variety-geo combination
+  # for limited-tastes, get a frequency scalar; and for other tastes, get an average vector
+  variety_geos = list(set(zip(wine_df['Variety'], wine_df['geo_normalized'])))
+  wine_variety_df = pd.DataFrame()
+  for v_g in variety_geos:
+    row_for_variety = get_variety_vectors_descriptors(v_g, wine_df, core_tastes, limited_tastes)
+    wine_variety_df = pd.concat([wine_variety_df, row_for_variety], axis=0, ignore_index=True)
+
+  # for non-aroma taste of each wine which still has a vector, we will do a PCA reduction to scalar, to align with limited tastes
+  for taste in core_tastes:
+    if taste in limited_tastes or taste == 'aroma':
+      continue
+    col_name = taste + ' vector'
+    pca = PCA(1)
+    wine_variety_df[col_name] = pca.fit_transform(list(wine_variety_df[col_name]))
+    wine_variety_df.rename(columns = {col_name: taste + ' scalar'}, inplace=True)
+
+
+  wine_variety_df.to_csv('processed_data/wine_variety_descriptor_vector.csv')
+  
+  
+  
 
   
 
